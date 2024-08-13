e812bed321adef67c9705e00cb990e7f8599197d
Kennyhunter254 <kipronokevin566@gmail.com>
2024-08-13 12:34:37 -0700

    React hooks app example

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -17,4 +17,8 @@ function TestComponent() {
   );
 }
 
+
+
+
+
 export default TestComponent;
---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestComponent() {
+  return <div title="time video">Now</div>;
+}
+
+export default TestComponent;
